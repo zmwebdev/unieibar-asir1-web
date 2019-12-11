8c512742213432802f389950910c5e812357cfb8
zmwebdev <zmwebdev@email.email>
2019-12-11 09:32:07 +0000

    html forms

--- 2Ebal/echo_all.php
+++ 2Ebal/echo_all.php
@@ -1,12 +1,10 @@
 <?php
 
-/*
-echo $_GET["firstname"];
-echo "<br>";
-echo $_GET["lastname"];
-*/
+var_dump($_GET);
 
+
+/*
 foreach ($_SERVER as $key => $val) 
     echo $key . "=>" . $val . "<br/>";
-
+*/
 //header("Location: new.html"); /* Redirect browser */
